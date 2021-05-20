51578e1372776724bea58c63459f21cb0df0354b
Mattizockt <ggurke2@gmail.com>
2021-05-20 10:24:51 +0200

    Navigation to racing page complete

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ def main():
     chrome_options = Options()
     driver = webdriver.Chrome("./chromedriver", options=chrome_options)
 
-    # navigating to main page
+    # navigating to racing page
     driver.get("https://play.typeracer.com/")
     assert "TypeRacer" in driver.title
     driver.implicitly_wait(5)
-
-    # navigating to
     driver.find_element_by_xpath("//a[normalize-space()='Enter a Typing Race']").click()
+
+    # wait till race can start
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='popupContent']//span["
                                                                               "@class='time']")))
-
     countdown = None
     while countdown != ":00":
         countdown = driver.find_element_by_xpath("//div[@class='popupContent']//span[@class='time']").text
@@ -32,6 +31,7 @@ if __name__ == "__main__":
     main()
 
     # //span
+    #
     # setTimeout(function(){ debugger; }, 1000);
     # you have to freeze the page with this code
     # than find out the xpath of timer by contains(text)
